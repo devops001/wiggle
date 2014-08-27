b9bab48b696122111087648d3545fcc771c0e01a
devops001 <devops001@gmail.com>
2014-08-27 12:57:35 -0700

    initShader() now sets up glsl variable stuff

--- wiggle.js
+++ wiggle.js
@@ -1,25 +1,21 @@
-///////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////
 // File:      wiggle.js
-// Purpose:   a few simple helper functions to get webgl up & running
+// Purpose:   A few simple helper functions to get webgl up & running
 // Author:    Ryan McDonald <devops001@gmail.com> 
 // Date:      2014.08.25
-// Note:      put shader code in html ids: vertex_shader, fragment_shader 
-// Functions: 
-//            initGL(gl,canvas)  //<- sets common gl flags
-//            initShader(gl)     //<- compiles & sets up the shader for use
-//            render()           //<- called 60 times per second. override it.
-//            startRendering()   //<- call this to start calling render().
-//            stopRendering()    //<- call this to stop calling render().
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
-function initGL(gl, canvas) {
+function initGL(canvas) {
+  var gl = canvas.getContext("webgl");
   gl.clearColor(0.2, 0.3, 0.4, 1.0);
   gl.enable(gl.DEPTH_TEST);
   gl.depthFunc(gl.LEQUAL);
   gl.viewport(0, 0, canvas.width, canvas.height);
+  return gl;
 }
 
-function initShader(gl) {
+function initShader(gl, canvas) {
+
   function createShader(shaderType, shaderSource) {
     var shader = gl.createShader(shaderType);
     gl.shaderSource(shader, shaderSource);
@@ -32,21 +28,51 @@ function initShader(gl) {
     }
     return shader;
   }
+
+  function setupShaderLocations(gl, shaderProgram) {
+    shaderProgram.loc = {};
+    shaderProgram.loc.position          = gl.getAttribLocation(shaderProgram,  "position");
+    shaderProgram.loc.modelViewMatrix   = gl.getUniformLocation(shaderProgram, "modelViewMatrix");
+    shaderProgram.loc.perspectiveMatrix = gl.getUniformLocation(shaderProgram, "perspectiveMatrix");
+    gl.enableVertexAttribArray(shaderProgram.loc.position);
+  }
+  
+  function setupShaderMatrices(shaderProgram, canvas) {
+    shaderProgram.mat4 = {};
+    shaderProgram.mat4.perspective = mat4.create();
+    shaderProgram.mat4.modelView   = mat4.create();
+    mat4.perspective(45.0, canvas.width/canvas.height, 0.1, 100.0, shaderProgram.mat4.perspective);
+    mat4.identity(shaderProgram.mat4.modelView);
+  }
+  
+  function setShaderUniformValues(gl, shaderProgram) {
+    gl.uniformMatrix4fv(shaderProgram.loc.modelViewMatrix,   false, shaderProgram.mat4.modelView);
+    gl.uniformMatrix4fv(shaderProgram.loc.perspectiveMatrix, false, shaderProgram.mat4.perspective);
+  }
+
   var vertexSource   = document.getElementById("vertex_shader").text;
   var fragmentSource = document.getElementById("fragment_shader").text;
   var vertexShader   = createShader(gl.VERTEX_SHADER, vertexSource);
   var fragmentShader = createShader(gl.FRAGMENT_SHADER, fragmentSource);
   var shaderProgram  = gl.createProgram();
+
   gl.attachShader(shaderProgram, vertexShader);
   gl.attachShader(shaderProgram, fragmentShader);
   gl.linkProgram(shaderProgram);
+
   if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
     var error = gl.getProgramInfoLog(shaderProgram);
     console.log("shader program failed to link: "+ error);
     gl.deleteProgram(shaderProgram);
     return null;
   }
+
   gl.useProgram(shaderProgram);
+
+  setupShaderLocations(gl, shaderProgram);
+  setupShaderMatrices(shaderProgram, canvas);
+  setShaderUniformValues(gl, shaderProgram);
+
   return shaderProgram;
 }
 
